9cf72b703089ffcaf4ac6fea0e0dc2aa6f4863a9
CrazyRikin <ranka.rikin.02@gmail.com>
2024-01-25 00:56:40 +0530

    HomePage Addition

---
+++ lib/utils/theme_data.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+var kColorTheme = ThemeData.from(
+  colorScheme: ColorScheme.fromSeed(
+      brightness: Brightness.dark,
+      seedColor: Colors.black
+      ),
+);
