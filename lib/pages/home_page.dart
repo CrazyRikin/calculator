003538f4443e1fa6bccd9dbbc244efc97296557f
CrazyRikin <ranka.rikin.02@gmail.com>
2024-01-25 02:14:58 +0530

    HomePageComplete . logic is yet to be added

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:calculator/utils/button_texts.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -17,13 +18,46 @@ class _HomePageState extends State<HomePage> {
       children: [
         Container(
           height: screenHeight * 0.34,
-          decoration:
-              const BoxDecoration(color: Color.fromARGB(255, 255, 68, 0)),
+          decoration: const BoxDecoration(color: Color.fromARGB(255, 0, 0, 0)),
         ),
         Container(
-          height: screenHeight * 0.66,
-          decoration: const BoxDecoration(color: Color.fromARGB(255, 0, 0, 0)),
-        )
+            padding: const EdgeInsets.all(16),
+            height: screenHeight * 0.66,
+            decoration: const BoxDecoration(
+                border: Border(
+                    top: BorderSide(color: Color.fromARGB(255, 122, 121, 121))),
+                color: Color.fromARGB(255, 5, 5, 5)),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Expanded(
+                  child: GridView.builder(
+                    itemCount: 16,
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 4),
+                    itemBuilder: (ctx, index) => Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextButton(
+                          onPressed: () {}, child: Text(buttonLabels[index])),
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.all(8),
+                  width: MediaQuery.of(context).size.width,
+                  child: TextButton(
+                    style: const ButtonStyle(
+                      shape: MaterialStatePropertyAll(RoundedRectangleBorder(
+                          borderRadius:
+                              BorderRadius.all(Radius.circular(500)))),
+                    ),
+                    onPressed: () {},
+                    child: const Text("="),
+                  ),
+                )
+              ],
+            ))
       ],
     ));
   }
--- lib/utils/theme_data.dart
+++ lib/utils/theme_data.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 
-var kColorTheme = ThemeData.from(
+var kColorTheme = ThemeData(
+  useMaterial3: true,
+  textButtonTheme: const TextButtonThemeData(
+      style: ButtonStyle(
+          shape: MaterialStatePropertyAll(CircleBorder(eccentricity: 0)),
+          padding: MaterialStatePropertyAll(EdgeInsets.all(8)),
+          foregroundColor:
+              MaterialStatePropertyAll(Color.fromARGB(202, 255, 255, 255)),
+          backgroundColor:
+              MaterialStatePropertyAll(Color.fromARGB(192, 75, 75, 75)))),
   colorScheme: ColorScheme.fromSeed(
-      brightness: Brightness.dark,
-      seedColor: Colors.black
-      ),
+      brightness: Brightness.dark, seedColor: Colors.black),
 );
