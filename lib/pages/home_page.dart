687f3334746a41855d32c57f0aa60f2c24fa12af
CrazyRikin <ranka.rikin.02@gmail.com>
2024-01-25 01:23:10 +0530

    dividedScreeninto1:2

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -10,6 +10,21 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("HomePage"));
+    final screenHeight = MediaQuery.of(context).size.height;
+
+    return Center(
+        child: Column(
+      children: [
+        Container(
+          height: screenHeight * 0.34,
+          decoration:
+              const BoxDecoration(color: Color.fromARGB(255, 255, 68, 0)),
+        ),
+        Container(
+          height: screenHeight * 0.66,
+          decoration: const BoxDecoration(color: Color.fromARGB(255, 0, 0, 0)),
+        )
+      ],
+    ));
   }
 }
