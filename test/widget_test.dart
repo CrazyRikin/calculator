ea53b4d3bb686920192b92da5a9d3e4a4370aca3
CrazyRikin <ranka.rikin.02@gmail.com>
2024-01-27 18:07:44 +0530

    test file modification

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,19 +9,17 @@ import 'package:calculator/pages/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:calculator/main.dart';
-
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const HomePage());
 
     // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
+    expect(find.text('c'), findsOneWidget);
     expect(find.text('1'), findsNothing);
 
     // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+    await tester.tap(find.byIcon(Icons.history));
     await tester.pump();
 
     // Verify that our counter has incremented.
